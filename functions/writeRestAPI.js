7ae5aa9901b9e9a8ae1ffde03698ab8c53c2ed3f
denisekgosnell <denisekgosnell@gmail.com>
2021-08-26 12:20:51 -0400

    dkg: updating to use rest client

--- functions/writeRestAPI.js
+++ functions/writeRestAPI.js
@@ -1,33 +1,27 @@
-const fetch = require('node-fetch')
+const { createClient } = require("@astrajs/rest");
 
-exports.handler = async function (event) {
-  const testNum = JSON.parse(event.body)
+// create an Astra client
+const astraClient = await createClient({
+  astraDatabaseId: process.env.ASTRA_DB_ID,
+  astraDatabaseRegion: process.env.ASTRA_DB_REGION,
+  applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
+});
 
-  const url = process.env.ASTRA_REST_ENDPOINT
-  const query = `{
-    test: ${JSON.stringify(testNum)},
-    time: ${JSON.stringify(Date.now())}
-  }`  
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json",
-      "x-cassandra-token": process.env.ASTRA_DB_APPLICATION_TOKEN
-    },
-    body: JSON.stringify({ query })
-  })
+const basePath = "/api/rest/v2/keyspaces/tablesGCP/resttest";
 
-  try {
-    const responseBody = await response.json()
-    return {
-      statusCode: 200,
-      body: JSON.stringify(responseBody)
-    }
-  } catch (e) {
-    console.log(e)
-    return {
-      statusCode: 500,
-      body: JSON.stringify(e)
-    }
-  }
+try {
+  // create a new user with a document id
+  const { data, status } = await astraClient.put(`${basePath}/1`, {
+    time: JSON.stringify(Date.now()),
+  });
+  return {
+    statusCode: 200,
+    body: JSON.stringify(responseBody),
+  };
+} catch (e) {
+  console.log(e);
+  return {
+    statusCode: 500,
+    body: JSON.stringify(e),
+  };
 }
