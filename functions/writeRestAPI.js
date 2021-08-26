eb43f6636b57fe8fb210e9eb81801c45e992a715
denisekgosnell <denisekgosnell@gmail.com>
2021-08-26 14:32:29 -0400

    dkg: updating for lots of iterations of many tests

--- functions/writeRestAPI.js
+++ functions/writeRestAPI.js
@@ -1,27 +1,30 @@
-const { createClient } = require("@astrajs/rest");
-
-// create an Astra client
-const astraClient = await createClient({
-  astraDatabaseId: process.env.ASTRA_DB_ID,
-  astraDatabaseRegion: process.env.ASTRA_DB_REGION,
-  applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
-});
-
-const basePath = "/api/rest/v2/keyspaces/tablesGCP/resttest";
-
-try {
-  // create a new user with a document id
-  const { data, status } = await astraClient.put(`${basePath}/1`, {
-    time: JSON.stringify(Date.now()),
+exports.handler = async function (event) {
+  const { createClient } = require("@astrajs/rest");
+  const testNum = JSON.parse(event.body);
+  // create an Astra client
+  const astraClient = await createClient({
+    astraDatabaseId: process.env.ASTRA_DB_ID,
+    astraDatabaseRegion: process.env.ASTRA_DB_REGION,
+    applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
   });
-  return {
-    statusCode: 200,
-    body: JSON.stringify(responseBody),
-  };
-} catch (e) {
-  console.log(e);
-  return {
-    statusCode: 500,
-    body: JSON.stringify(e),
-  };
-}
+
+  const basePath = "/api/rest/v2/keyspaces/tablesGCP/resttest";
+
+  try {
+    // create a new user with a document id
+    const isoDate = new Date().toISOString();
+    const { data, status } = await astraClient.put(`${basePath}/${testNum}`, {
+      time: isoDate,
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (e) {
+    console.log(e);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(e),
+    };
+  }
+};
